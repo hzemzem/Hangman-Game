6226f5f12379aa2f3e2224a5e6db77193cb3895e
anas tellat <anas tellat>
2017-05-15 01:45:39 -0400

    initial commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,141 @@
-var byline = document.getElementById('byline');     // Find the H2
-bylineText = byline.innerHTML;                                      // Get the content of the H2
-bylineArr = bylineText.split('');                                   // Split content into array
-byline.innerHTML = '';                                                      // Empty current content
+window.onload = function () {
 
-var span;                   // Create variables to create elements
-var letter;
+  var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  
+  var categories;         
+  var chosenCategory;    
+  var word ;             
+  var guess ;            
+  var geusses = [ ];     
+  var lives ;            
+  var counter ;           
+  var space;             
 
-for(i=0;i<bylineArr.length;i++){                                    // Loop for every letter
-  span = document.createElement("span");                    // Create a <span> element
-  letter = document.createTextNode(bylineArr[i]);   // Create the letter
-  if(bylineArr[i] == ' ') {                                             // If the letter is a space...
-    byline.appendChild(letter);                 // ...Add the space without a span
-  } else {
-        span.appendChild(letter);                       // Add the letter to the span
-    byline.appendChild(span);                   // Add the span to the h2
+  // Get elements
+  var showLives = document.getElementById("mylives");
+  var showCatagory = document.getElementById("scatagory");
+
+
+
+  // create alphabet ul
+  var buttons = function () {
+    myButtons = document.getElementById('buttons');
+    letters = document.createElement('ul');
+
+    for (var i = 0; i < alphabet.length; i++) {
+      letters.id = 'alphabet';
+      list = document.createElement('li');
+      list.id = 'letter';
+      list.innerHTML = alphabet[i];
+      check();
+      myButtons.appendChild(letters);
+      letters.appendChild(list);
+    }
+  }
+    
+  
+  // Select Catagory
+  var selectCat = function () {
+   	if (chosenCategory === categories[0]) {
+      catagoryName.innerHTML = "The Chosen Category Is Star Wars Characters";
+    } else if (chosenCategory === categories[1]) {
+      catagoryName.innerHTML = "The Chosen Category Is Star Wars Locations";
+    }
+  }
+
+  // Create guesses ul
+   result = function () {
+    wordHolder = document.getElementById('hold');
+    correct = document.createElement('ul');
+
+    for (var i = 0; i < word.length; i++) {
+      correct.setAttribute('id', 'my-word');
+      guess = document.createElement('li');
+      guess.setAttribute('class', 'guess');
+      if (word[i] === "-") {
+        guess.innerHTML = "-";
+        space = 1;
+      } else {
+        guess.innerHTML = "_";
+      }
+
+      geusses.push(guess);
+      wordHolder.appendChild(correct);
+      correct.appendChild(guess);
+    }
+  }
+  
+  // Show lives
+   comments = function () {
+    showLives.innerHTML = "You have " + lives + " lives";
+    if (lives < 1) {
+      showLives.innerHTML = "Game Over";
+    }
+    for (var i = 0; i < geusses.length; i++) {
+      if (counter + space === geusses.length) {
+        showLives.innerHTML = "You Win!";
+      }
+    }
+  }
+
+
+  // OnClick Function
+   check = function () {
+    list.onclick = function () {
+      var geuss = (this.innerHTML);
+      this.setAttribute("class", "active");
+      this.onclick = null;
+      for (var i = 0; i < word.length; i++) {
+        if (word[i] === geuss) {
+          geusses[i].innerHTML = geuss;
+          counter += 1;
+        } 
+      }
+      var j = (word.indexOf(geuss));
+      if (j === -1) {
+        lives -= 1;
+        comments();
+      } else {
+        comments();
+      }
+    }
+  }
+  
+  // Play
+  play = function () {
+    categories = [
+        ["chewbacca", "han-solo", "luke-skywalker", "darth-vader", "leia-organa", "obi-wan-kenobi", "mace-windu", "stormtroopers"],
+        ["endor", "coruscant", "kamino", "Mustafar"]
+    ];
+
+
+    chosenCategory = categories[Math.floor(Math.random() * categories.length)];
+    word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+    word = word.replace(/\s/g, "-");
+    console.log(word);
+    buttons();
+
+    geusses = [ ];
+    lives = 10;
+    counter = 0;
+    space = 0;
+    result();
+    comments();
+    selectCat();
+  }
+
+
+
+  play();
+  
+
+   // Reset
+
+  document.getElementById('reset').onclick = function() {
+    correct.parentNode.removeChild(correct);
+    letters.parentNode.removeChild(letters);
+    play();
   }
 }
